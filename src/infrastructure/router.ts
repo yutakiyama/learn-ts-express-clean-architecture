d57b8b4c7d259dfa70077828da62353ba51714de
Yuta Kiyama <albero.montagna@gmail.com>
2021-04-07 18:55:41 +0900

    feat: タスク作成のrouteを実装

--- src/infrastructure/router.ts
+++ src/infrastructure/router.ts
@@ -1,18 +1,15 @@
 import Express from 'express';
+import { TasksController } from '@/interfaces/controllers/TasksController';
+import { MysqlConnection } from '@/infrastructure/MysqlConnection';
 
 export const router = Express.Router();
 
-type Task = {
-  id: number;
-  title: string;
-}
+const mysqlConnection = new MysqlConnection();
+const tasksController = new TasksController(mysqlConnection);
 
-const tasks: Task[] = [
-  {id: 1, title: 'タスク1'},
-  {id: 2, title: 'タスク2'},
-  {id: 3, title: 'タスク3'},
-]
+// router.get('/tasks', (req, res) => res.send(JSON.stringify(tasks)));
 
-router.get('/tasks',  ((req, res) => (
-  res.send(JSON.stringify(tasks))
-)))
+router.post('/tasks', async (req, res) => {
+  const result = await tasksController.createTask(req);
+  res.send(result);
+});
